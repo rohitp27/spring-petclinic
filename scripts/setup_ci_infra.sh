64ab422dcf56cd5fe14fcc0b8c722afde912d7e8
Rohit Pandey <rohit.pandey.h@gmail.com>
2022-07-27 23:39:10 -0400

    Update setup_ci_infra.sh

--- scripts/setup_ci_infra.sh
+++ scripts/setup_ci_infra.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
 
+cd $PROJECT_HOME
+
+sudo bash ./setup_sonarqube.sh
+
+echo "--------------------------------"
+echo "--------------------------------"
+echo "--------------------------------"
+
+sudo bash ./setup_jenkins_with_blueocean.sh
