45e7913cbcf3d71637540a283d08c2fa0f2f9d33
Rohit Pandey <rohit.pandey.h@gmail.com>
2022-07-27 20:28:04 -0400

    Update setup_jenkins_with_blueocean.sh

--- scripts/setup_jenkins_with_blueocean.sh
+++ scripts/setup_jenkins_with_blueocean.sh
@@ -1,1 +1,43 @@
+#!/usr/bin/env bash
 
+####Create a common network
+docker network create jenkins
+
+#run jenkins in a docker container
+docker run \
+  --name jenkins-docker \
+  --rm \
+  --detach \
+  --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  --publish 3000:3000 \
+  docker:dind \
+  --storage-driver overlay2 
+
+#Run the dockerfile
+docker build -t myjenkins-blueocean:2.346.2-1 .
+
+docker run \
+  --name jenkins-blueocean \
+  --detach \
+  --network jenkins \
+  --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client \
+  --env DOCKER_TLS_VERIFY=1 \
+  --publish 8080:8080 \
+  --publish 50000:50000 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  --volume "$HOME":/home \
+  --restart=on-failure \
+  --env JAVA_OPTS="-Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true" \
+  myjenkins-blueocean:2.346.2-1 
+
+
+#Print the command password
+sudo docker exec -it jenkins-blueocean /bin/bash -c \ "cat /var/jenkins_home/secrets/initialAdminPassword"
