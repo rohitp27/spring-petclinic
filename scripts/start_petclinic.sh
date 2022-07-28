19e4d24ff97b94e3edce18ba78bd0f6b95734fd2
Rohit Pandey <rohit.pandey.h@gmail.com>
2022-07-27 20:30:55 -0400

    Update start_petclinic.sh

--- scripts/start_petclinic.sh
+++ scripts/start_petclinic.sh
@@ -1,1 +1,11 @@
+#!/usr/bin/env bash
 
+
+#Create directory in /bin
+sudo cd -p /bin/spring-petclinic
+
+#Copy from docker volume of the jenkins container to the local
+sudo docker cp jenkins-blueocean:/var/jenkins_home/workspace/spring-petclinic/target/spring-petclinic-2.7.0-SNAPSHOT.jar .
+
+#Run the jar file
+java -jar ./spring-petclinic-2.7.0-SNAPSHOT.jar 
