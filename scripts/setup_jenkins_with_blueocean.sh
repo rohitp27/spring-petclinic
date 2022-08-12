87395780535f2aeb5ce3acd56cae172de577a16f
rohitp27 <rohit.pandey.h@gmail.com>
2022-08-11 22:35:45 -0400

    asdsa

--- scripts/setup_jenkins_with_blueocean.sh
+++ scripts/setup_jenkins_with_blueocean.sh
@@ -17,7 +17,9 @@ docker run \
   --publish 2376:2376 \
   --publish 3000:3000 \
   docker:dind \
-  --storage-driver overlay2 
+  --storage-driver overlay2 \
+  --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" \
+  --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)"
 
 #Run the dockerfile
 docker build -t myjenkins-blueocean:2.346.2-1 .
@@ -36,7 +38,9 @@ docker run \
   --volume "$HOME":/home \
   --restart=on-failure \
   --env JAVA_OPTS="-Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true" \
-  myjenkins-blueocean:2.346.2-1 
+  myjenkins-blueocean:2.346.2-1 \
+  --build-arg ssh_prv_key="$(cat ~/.ssh/id_rsa)" \
+  --build-arg ssh_pub_key="$(cat ~/.ssh/id_rsa.pub)"
 
 
 #Print the command password
